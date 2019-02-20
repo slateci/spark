9f476d57a476a08b116563e34f8441dff49cce85
Ilija Vukotic <ivukotic@cern.ch>
2019-02-20 15:11:21 -0600

    +x

--- test_pi.py
+++ test_pi.py
@@ -9,9 +9,41 @@ sc = SparkContext(conf=conf)
 
 NUM_SAMPLES = 3000000
 
+
 def inside(p):
     x, y = random.random(), random.random()
     return x * x + y * y < 1
 
+
 count = sc.parallelize(range(0, NUM_SAMPLES)).filter(inside).count()
 print("Pi is roughly ",  (4.0 * count / NUM_SAMPLES))
+
+
+from pyspark.sql import SparkSession
+from operator import add
+from random import random
+
+import os
+os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3.5'
+os.environ["PYSPARK_DRIVER_PYTHON"]='/usr/local/bin/ipython3'
+
+
+spark = SparkSession\
+    .builder\
+    .appName("PythonPi")\
+    .getOrCreate()
+
+partitions = 2
+n = 100000 * partitions
+
+
+def f(_):
+    x = random() * 2 - 1
+    y = random() * 2 - 1
+    return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+
+count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+print("Pi is roughly %f" % (4.0 * count / n))
+
+spark.stop()
